50d41ea612502d93916340429e6c286e0e92b875
Monika Grzanek <grzanekmonika@o2.pl>
2025-05-08 12:55:31 +0200

    Add orders module with endpoints

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { OrdersService } from './orders.service';
 
 @Controller('orders')
-export class OrdersController {}
+export class OrdersController {
+  constructor(private orderService: OrdersService) {}
+  @Get('/')
+  getAll(): any {
+    return this.orderService.getAll();
+  }
+  @Get('/:id')
+  getById(@Param('id') id: string) {
+    return this.orderService.getById(id);
+  }
+}
