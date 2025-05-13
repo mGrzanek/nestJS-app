f47e4f42a11f93512f4f9f8c664368d744cf5a2d
Monika Grzanek <grzanekmonika@o2.pl>
2025-05-13 15:14:46 +0200

    Add relation Order - Product

--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,5 +1,4 @@
-import { Injectable } from '@nestjs/common';
-import { v4 as uuidv4 } from 'uuid';
+import { BadRequestException, Injectable } from '@nestjs/common';
 import { Order } from '@prisma/client';
 import { PrismaService } from 'src/shared/services/prisma.service';
 
@@ -7,28 +6,47 @@ import { PrismaService } from 'src/shared/services/prisma.service';
 export class OrdersService {
   constructor(private prismaService: PrismaService) {}
   public getAll(): Promise<Order[]> {
-    return this.prismaService.order.findMany();
+    return this.prismaService.order.findMany({ include: { product: true } });
   }
   public getById(id: Order['id']): Promise<Order | null> {
     return this.prismaService.order.findUnique({
       where: { id },
+      include: { product: true },
     });
   }
-  public create(
+  public async create(
     orderData: Omit<Order, 'id' | 'createdAt' | 'updatedAt'>,
   ): Promise<Order> {
-    const newOrder = { ...orderData, id: uuidv4() };
-    return this.prismaService.order.create({
-      data: newOrder,
-    });
+    const { productId, ...otherData } = orderData;
+    try {
+      return await this.prismaService.order.create({
+        data: {
+          ...otherData,
+          product: {
+            connect: { id: productId },
+          },
+        },
+      });
+    } catch (error) {
+      if (error.code === 'P2025')
+        throw new BadRequestException("Product doesn't exist");
+      throw error;
+    }
   }
+
   public update(
     id: Order['id'],
     orderData: Omit<Order, 'id' | 'createdAt' | 'updatedAt'>,
   ): Promise<Order> {
+    const { productId, ...otherData } = orderData;
     return this.prismaService.order.update({
       where: { id },
-      data: orderData,
+      data: {
+        ...otherData,
+        product: {
+          connect: { id: productId },
+        },
+      },
     });
   }
   public remove(id: Order['id']): Promise<Order> {
